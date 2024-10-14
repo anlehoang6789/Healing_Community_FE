24c744fc4b866b58c33800496fdc7f8ba351a402
anlehoang6789 <anlehoang6789@gmail.com>
2024-10-14 15:03:34 +0700

    ui: take the test for user

--- src/components/ui/radio-group.tsx
+++ src/components/ui/radio-group.tsx
@@ -22,19 +22,21 @@ RadioGroup.displayName = RadioGroupPrimitive.Root.displayName;
 
 const RadioGroupItem = React.forwardRef<
   React.ElementRef<typeof RadioGroupPrimitive.Item>,
-  React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item>
->(({ className, ...props }, ref) => {
+  React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item> & {
+    dotColor?: string;
+  }
+>(({ className, dotColor = "currentColor", ...props }, ref) => {
   return (
     <RadioGroupPrimitive.Item
       ref={ref}
       className={cn(
-        "aspect-square h-4 w-4 rounded-full border border-primary text-primary shadow focus:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+        "aspect-square h-5 w-5 rounded-full border border-primary text-primary shadow focus:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
       {...props}
     >
       <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
-        <Dot className="h-3.5 w-3.5 fill-primary" strokeWidth={"3px"} />
+        <Dot className="h-4 w-4" strokeWidth={"20px"} color={dotColor} />
       </RadioGroupPrimitive.Indicator>
     </RadioGroupPrimitive.Item>
   );
