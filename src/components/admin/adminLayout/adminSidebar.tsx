c65b14e94ae1b9a31fadad8c4fd4732469fc4280
anlehoang6789 <anlehoang6789@gmail.com>
2024-10-20 21:25:51 +0700

    ui: config layout for admin

---
+++ src/components/admin/adminLayout/adminSidebar.tsx
@@ -0,0 +1,82 @@
+"use client";
+import adminItems from "@/components/admin/adminLayout/adminItems";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  TooltipProvider,
+} from "@/components/ui/tooltip";
+import { cn } from "@/lib/utils";
+import { Settings } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function AdminSidebar() {
+  const pathname = usePathname();
+
+  return (
+    <TooltipProvider>
+      <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
+        <nav className="flex flex-col items-center gap-4 px-2 py-4">
+          <Link
+            href="/moderator/manage-stories"
+            className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
+          >
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/healing-community.appspot.com/o/banner%2Flotus-login.jpg?alt=media&token=b948162c-1908-43c1-8307-53ea209efc4d"
+              alt="logo"
+              sizes="icon"
+            />
+            <span className="sr-only">Acme Inc</span>
+          </Link>
+
+          {adminItems.map((Item, index) => {
+            const isActive = pathname === Item.href;
+            return (
+              <Tooltip key={index}>
+                <TooltipTrigger asChild>
+                  <Link
+                    href={Item.href}
+                    className={cn(
+                      "flex h-9 w-9 items-center justify-center rounded-lg transition-colors hover:text-accent-foreground md:h-8 md:w-8",
+                      {
+                        "bg-accent text-accent-foreground": isActive,
+                        "text-muted-foreground": !isActive,
+                      }
+                    )}
+                  >
+                    <Item.Icon className="h-5 w-5" />
+                    <span className="sr-only">{Item.title}</span>
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent side="right">{Item.title}</TooltipContent>
+              </Tooltip>
+            );
+          })}
+        </nav>
+        {/* nút cài đặt nằm bên dưới của thanh sidebar */}
+        <nav className="mt-auto flex flex-col items-center gap-4 px-2 py-4">
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Link
+                href="/manage/setting"
+                className={cn(
+                  "flex h-9 w-9 items-center justify-center rounded-lg  transition-colors hover:text-foreground md:h-8 md:w-8",
+                  {
+                    "bg-accent text-accent-foreground":
+                      pathname === "/manage/setting",
+                    "text-muted-foreground": pathname !== "/manage/setting",
+                  }
+                )}
+              >
+                <Settings className="h-5 w-5" />
+                <span className="sr-only">Cài đặt</span>
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent side="right">Cài đặt</TooltipContent>
+          </Tooltip>
+        </nav>
+      </aside>
+    </TooltipProvider>
+  );
+}
