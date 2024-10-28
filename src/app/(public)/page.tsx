68ad300c32ed58d80522e352d548f0c7c00afd0d
giaminh05 <hagiaminh29062002@gmail.com>
2024-10-28 22:25:36 +0700

    UI: fix bug in consultation calendar

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -4,11 +4,14 @@ import HotPosts from "@/components/homePage/hotPosts";
 import Posts from "@/components/homePage/posts";
 import QuickPosts from "@/components/homePage/quickPosts";
 import React from "react";
+import { cookies } from "next/headers";
 
-export default function page() {
+export default function HomePage() {
+  const cookieStore = cookies();
+  const isAuth = Boolean(cookieStore.get("accessToken")?.value);
   return (
     <div>
-      <Banner />
+      {!isAuth ? <Banner /> : <div className="mt-6"></div>}
       <div className="w-full bg-background h-auto overflow-hidden flex flex-1 flex-col gap-4 p-4 pt-0 md:pt-0 md:gap-8 md:p-8">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
           <div className="md:col-span-2 col-span-3 order-1 md:order-1">
