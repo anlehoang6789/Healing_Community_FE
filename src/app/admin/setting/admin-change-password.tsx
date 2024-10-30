6c06cbe28546e8dc220dd771fe36ee8cfc5e0eca
anlehoang6789 <anlehoang6789@gmail.com>
2024-10-30 21:16:52 +0700

    ui: change password and change information for 2 roles(admin, moderator)

---
+++ src/app/admin/setting/admin-change-password.tsx
@@ -0,0 +1,130 @@
+"use client";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useForm } from "react-hook-form";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import {
+  ChangePasswordBody,
+  ChangePasswordBodyType,
+} from "@/schemaValidations/account.schema";
+
+export default function AdminChangePassword() {
+  const form = useForm<ChangePasswordBodyType>({
+    resolver: zodResolver(ChangePasswordBody),
+    defaultValues: {
+      currentPassword: "",
+      newPassword: "",
+      confirmNewPassword: "",
+    },
+  });
+
+  return (
+    <Form {...form}>
+      <form
+        noValidate
+        className="grid auto-rows-max items-start gap-4 md:gap-8"
+        // onSubmit={form.handleSubmit(onSubmit, (err) => console.log(err))}
+        // onReset={resetForm}
+      >
+        <Card className="overflow-hidden" x-chunk="dashboard-07-chunk-4">
+          <CardHeader>
+            <CardTitle className="text-muted-foreground">
+              Đổi mật khẩu
+            </CardTitle>
+            <CardDescription className="sr-only">
+              Lipsum dolor sit amet, consectetur adipiscing elit
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="grid gap-6">
+              <FormField
+                control={form.control}
+                name="currentPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="grid gap-3 text-muted-foreground">
+                      <Label htmlFor="currentPassword">Mật khẩu cũ</Label>
+                      <Input
+                        id="currentPassword"
+                        type="password"
+                        className="w-full"
+                        {...field}
+                      />
+                      <FormMessage />
+                    </div>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="newPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="grid gap-3 text-muted-foreground">
+                      <Label htmlFor="newPassword">Mật khẩu mới</Label>
+                      <Input
+                        id="newPassword"
+                        type="password"
+                        className="w-full"
+                        {...field}
+                      />
+                      <FormMessage />
+                    </div>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="confirmNewPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="grid gap-3 text-muted-foreground">
+                      <Label htmlFor="confirmNewPassword">
+                        Nhập lại mật khẩu mới
+                      </Label>
+                      <Input
+                        id="confirmNewPassword"
+                        type="password"
+                        className="w-full"
+                        {...field}
+                      />
+                      <FormMessage />
+                    </div>
+                  </FormItem>
+                )}
+              />
+              <div className=" items-center gap-2 md:ml-auto flex">
+                <Button
+                  variant="ghost"
+                  size="sm"
+                  type="reset"
+                  className="text-muted-foreground"
+                >
+                  Hủy
+                </Button>
+                <Button
+                  size="sm"
+                  type="submit"
+                  variant={"outline"}
+                  className="bg-backgroundChat text-textChat"
+                >
+                  Lưu thông tin
+                </Button>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      </form>
+    </Form>
+  );
+}
