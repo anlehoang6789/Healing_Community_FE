2b66406719981bab1b79099c05a22ef08c2d1baf
anlehoang6789 <anlehoang6789@gmail.com>
2024-11-06 17:47:14 +0700

    feat: call api forgot password

--- src/apiRequests/account.ts
+++ src/apiRequests/account.ts
@@ -1,9 +1,20 @@
 import http from "@/lib/http";
-import { ChangePasswordBodyType } from "@/schemaValidations/account.schema";
+import {
+  ChangePasswordBodyType,
+  ForgotPasswordBodyType,
+  ResetPasswordWithOtpBodyType,
+} from "@/schemaValidations/account.schema";
 
 const accountApiRequest = {
   changePasswordForUser: (body: ChangePasswordBodyType) =>
     http.post<{ message: string }>("user/reset-password", body),
+  forgotPasswordSendOtp: (body: ForgotPasswordBodyType) =>
+    http.post<{ message: string }>("forgotpassword/forgot-password", body),
+  resetPasswordWhenHaveOtp: (body: ResetPasswordWithOtpBodyType) =>
+    http.post<{ message: string }>(
+      "forgotpassword/confirm-forgot-password",
+      body
+    ),
 };
 
 export default accountApiRequest;
--- src/schemaValidations/auth.schema.ts
+++ src/schemaValidations/auth.schema.ts
@@ -4,7 +4,7 @@ import z from "zod";
 export const LoginBody = z
   .object({
     email: z.string().email(),
-    password: z.string().min(6).max(100),
+    password: z.string().min(8).max(100),
   })
   .strict();
 
