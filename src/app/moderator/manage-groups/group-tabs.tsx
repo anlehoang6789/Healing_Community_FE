9626dd753fdd72df6ea7466606204c5e807ce2c4
giaminh05 <hagiaminh29062002@gmail.com>
2024-11-13 15:09:29 +0700

    UI: add pagination for manage list of group

--- src/app/moderator/manage-groups/group-tabs.tsx
+++ src/app/moderator/manage-groups/group-tabs.tsx
@@ -1,16 +1,23 @@
 import ListOfGroups from "@/app/moderator/manage-groups/list-of-groups";
 import RequestGroup from "@/app/moderator/manage-groups/request-group";
+import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Plus } from "lucide-react";
 import React from "react";
 
 export default function GroupTabs() {
   return (
     <Tabs defaultValue="listGroup" className="space-y-4">
-      <TabsList>
-        <TabsTrigger value="listGroup">Danh sách nhóm</TabsTrigger>
-        <TabsTrigger value="requestGroup">Yêu cầu</TabsTrigger>
-      </TabsList>
-
+      <div className="flex items-center justify-between">
+        <TabsList>
+          <TabsTrigger value="listGroup">Danh sách nhóm</TabsTrigger>
+          <TabsTrigger value="requestGroup">Yêu cầu</TabsTrigger>
+        </TabsList>
+        <Button variant={"outline"}>
+          <Plus className="mr-2 h-4 w-4" />
+          <span>Tạo nhóm</span>
+        </Button>
+      </div>
       <TabsContent value="listGroup" className="space-y-4">
         <div>
           <ListOfGroups />
