9812671b30b9146b145c47a76084701fe1373bd6
anlehoang6789 <anlehoang6789@gmail.com>
2024-11-19 14:44:27 +0700

    fix bug path navigation in middleware

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,10 +1,5 @@
 import Banner from "@/components/landingPage/banner";
-import HighlightPosts from "@/components/homePage/highlightPosts";
-import HotPosts from "@/components/homePage/hotPosts";
-import Posts from "@/components/homePage/posts";
-import QuickPosts from "@/components/homePage/quickPosts";
 import React from "react";
-import { cookies } from "next/headers";
 
 export default function LandingPage() {
   return (
