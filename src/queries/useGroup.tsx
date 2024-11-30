9ddf486c194e88c46ed99f4e8d42e35277cc6fae
giaminh05 <hagiaminh29062002@gmail.com>
2024-11-30 10:14:52 +0700

    Feat: get all groups

---
+++ src/queries/useGroup.tsx
@@ -0,0 +1,9 @@
+import groupApiRequest from "@/apiRequests/group";
+import { useQuery } from "@tanstack/react-query";
+
+export const useGetAllGroupsQuery = () => {
+  return useQuery({
+    queryKey: ["get-all-groups"],
+    queryFn: groupApiRequest.getAllGroups,
+  });
+};
