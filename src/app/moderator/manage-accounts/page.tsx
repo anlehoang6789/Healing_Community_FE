fd087f26578ea08bf6122b148a586631edb84caa
anlehoang6789 <anlehoang6789@gmail.com>
2025-01-01 15:38:45 +0700

    feat: manage account for moderator

--- src/app/moderator/manage-accounts/page.tsx
+++ src/app/moderator/manage-accounts/page.tsx
@@ -1,25 +1,17 @@
-"use client";
-import FilterUser from "@/app/moderator/manage-accounts/filter-user";
 import TableListUser from "@/app/moderator/manage-accounts/table-list-user";
-import React, { useState } from "react";
+import { Suspense } from "react";
 
 export default function ManageAccounts() {
-  const [filters, setFilters] = useState({ role: "all", status: "all" });
-
-  const handleFilterChange = (newFilters: { role: string; status: string }) => {
-    setFilters(newFilters);
-  };
   return (
     <div className="container mx-auto p-4">
-      <div className="flex flex-col md:flex-row items-center justify-around mb-4">
+      <div className="flex flex-col md:flex-row items-center mb-4">
         <h1 className="text-2xl font-bold text-muted-foreground">
           Quản lý tài khoản người dùng
         </h1>
-        {/* Filter user */}
-        <FilterUser onFilterChange={handleFilterChange} />
       </div>
-      {/* Table data with pagination */}
-      <TableListUser filters={filters} />
+      <Suspense>
+        <TableListUser />
+      </Suspense>
     </div>
   );
 }
