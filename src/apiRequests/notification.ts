ccea2d266879da78ff30589c8f07b60944416154
anlehoang6789 <anlehoang6789@gmail.com>
2025-01-18 17:26:55 +0700

    feat: get notification

--- src/apiRequests/notification.ts
+++ src/apiRequests/notification.ts
@@ -1,10 +1,22 @@
 import http from "@/lib/http";
-import { UnreadCountResponseType } from "@/schemaValidations/notification.schema";
+import {
+  GetNotificationListResponseType,
+  UnreadCountResponseType,
+} from "@/schemaValidations/notification.schema";
 
 const notificationApiRequest = {
   getUnreadCount: () =>
     http.get<UnreadCountResponseType>(
       "notification/api/notification/unread-count"
+    ),
+  getNotification: () =>
+    http.get<GetNotificationListResponseType>(
+      "notification/api/notification/notifications"
+    ),
+  maskAsReadNotification: (notificationId: string) =>
+    http.post<{ message: string }>(
+      `notification/api/notification/mark-as-read/${notificationId}`,
+      {}
     ),
 };
 
